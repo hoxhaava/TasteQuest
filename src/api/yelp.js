523ac7f7a886c5a7e7c0808db1408e119e88aea5
Hava Hoxha <60502737+hoxhaava@users.noreply.github.com>
2024-01-17 21:00:19 +0100

    Update yelp.js

--- src/api/yelp.js
+++ src/api/yelp.js
@@ -1,9 +1,9 @@
 import axios from 'axios'
 
 export default axios.create({
-    baseURL: 'https://api.yelp.com/v3/businesses',
+    baseURL: process.env.YELP_API,
     headers: {
-        Authorization: 'Bearer UlNAGuqe2XM1PQBfQfGbvioN7EBS1EHDknNhmIxgw5ocgibzNpRYngiX9Ezz0j5Suy_ZLJytYZSomTqOh-s6khrRlqhcyeeFz1GZJVX8_l-fHkBrkNG_O2pVQAynY3Yx'
+        Authorization: process.env.TOKEN
     }
 })
 
